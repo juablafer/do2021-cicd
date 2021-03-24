945abd299b56b194b5bc17fd684ec073d1917979
Juan Miguel <juanmiblancof@gmail.com>
2021-03-24 21:22:19 +0100

    terraform

---
+++ deploy/index.js
@@ -0,0 +1,13 @@
+// const fs = require('fs');
+// const path = require('path');
+
+// if (process.env.KEY_PEM == undefined) {
+//     console.error("KEY_PEM not defined");
+//     process.exit(1);
+// } else {
+//     let pemFileName = 'deploy/key.pem';
+
+//     try {
+//         fs.writeFileSync(pemFileName, process.env.KEY_PEM)
+//     }
+// }
